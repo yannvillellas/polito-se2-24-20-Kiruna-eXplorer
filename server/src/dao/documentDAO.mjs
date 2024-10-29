07e1a830fd51318ef97f2e26b8e003ba9f90fbfe
ChristianGalipo1 <s331996@studenti.polito.it>
2024-10-29 11:38:24 +0100

    dao method for the story4

--- server/src/dao/documentDAO.mjs
+++ server/src/dao/documentDAO.mjs
@@ -1,2 +1,15 @@
 import {db} from "../database/db.mjs"
 import Document from "../models/document.mjs"
+
+export const listDocuments = () => {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM Document", (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                const documents = rows.map(row => new Document(row.docId, row.title, row.description, row.stackeholders, row.scale, row.issuanceDate, row.type, row.connections, row.language, row.pages));
+                resolve(documents);
+            }
+        });
+    });
+}
