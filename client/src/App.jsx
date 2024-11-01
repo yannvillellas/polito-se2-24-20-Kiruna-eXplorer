59024297f3de6d6f22e403bcbefa76bf655fe702
Royahp <roya89.hosseinpour@gmail.com>
2024-11-01 19:25:56 +0100

    fistpage added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState, useEffect } from 'react'
-import {Route, Routes, Navigate} from 'react-router-dom'
-import Map from './components/map/Map'
-import Login from './components/login/Login'
-
-
+import { useState, useEffect } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Row, Col, Button, Form } from "react-bootstrap";
+import Map from "./components/map/Map";
+import Login from "./components/login/Login";
+import FirstPage from "./components/firstPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  useEffect(()=>{},[]);
+  useEffect(() => {}, []);
 
   return (
     <Routes>
-      <Route path='/*'/>
-      <Route path='/' Component={Map}/>
-      <Route path='/login' Component={Login}/>
+      <Route path="/*" />
+      <Route path="/" Component={Map} />
+      <Route path="/login" Component={Login} />
+      <Route path="/firstPage" Component={FirstPage} />
     </Routes>
-  )
+  );
 }
 
 export default App;
