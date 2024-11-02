68fc256e3de8425b0fb8fbc2cc21c98957cd4117
Ludwig Lenzini s331395 <s331395@studenti.polito.it>
2024-11-02 16:13:54 +0100

    Revert "add firstPage" reverted This reverts commit 001eb3bf6e07d03c431911bd577821f75cf1c1a6.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import Map from "./components/map/Map";
 import Login from "./components/login/Login";
 import FirstPage from "./components/firstPage";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 function App() {
   useEffect(() => {}, []);
 
