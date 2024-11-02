61b6615a9014aee9492286188e7b5baa25163d07
Ludwig Lenzini s331395 <s331395@studenti.polito.it>
2024-11-02 16:14:28 +0100

    Revert "fistpage added" reverting This reverts commit 59024297f3de6d6f22e403bcbefa76bf655fe702.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,19 @@
-import { useState, useEffect } from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
-import { Row, Col, Button, Form } from "react-bootstrap";
-import Map from "./components/map/Map";
-import Login from "./components/login/Login";
-import FirstPage from "./components/firstPage";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from 'react'
+import {Route, Routes, Navigate} from 'react-router-dom'
+import Map from './components/map/Map'
+import Login from './components/login/Login'
+
+
 function App() {
-  useEffect(() => {}, []);
+  useEffect(()=>{},[]);
 
   return (
     <Routes>
-      <Route path="/*" />
-      <Route path="/" Component={Map} />
-      <Route path="/login" Component={Login} />
-      <Route path="/firstPage" Component={FirstPage} />
+      <Route path='/*'/>
+      <Route path='/' Component={Map}/>
+      <Route path='/login' Component={Login}/>
     </Routes>
-  );
+  )
 }
 
 export default App;
