56eceac6f126f359bba31914cff71c8b80f3b146
ChristianGalipo1 <s331996@studenti.polito.it>
2024-11-17 10:53:09 +0100

    modifiche app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,22 @@
 import { useState, useEffect } from 'react';
-import { Routes, Route, Outlet, Navigate, useNavigate  } from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate, useNavigate } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import PageNotFound from './components/pageNotFound/PageNotFound';
 import Map from './components/map/Map';
 import Login from './components/login/Login';
-import Link from './components/link/Link';
 import Header from './components/header/Header';
 import AuthAPI from './api/authAPI';
 
 import './App.css';
 
 function App() {
-    const [user, setUser] = useState(null);  // Consolidato `user` e `userRole`
+    const [user, setUser] = useState(null);
     const [authChecked, setAuthChecked] = useState(false);
     const [loggedIn, setLoggedIn] = useState(false);
+    const [isUrbanPlanner, setIsUrbanPlanner] = useState(false);
     const navigate = useNavigate();
 
-
-
+    // Check authentication
     useEffect(() => {
         const checkAuth = async () => {
             try {
@@ -25,11 +24,12 @@ function App() {
                 if (user) {
                     setLoggedIn(true);
                     setUser(user);
+                    setIsUrbanPlanner(user.role === 'urban_planner');
                 } else {
                     console.log('User is not logged in');
                 }
             } catch (error) {
-                console.error("Errore durante l'autenticazione:", error);
+                console.error("Authentication error:", error);
                 setUser(null);
             } finally {
                 setAuthChecked(true);
@@ -45,28 +45,26 @@ function App() {
             if (user) {
                 setLoggedIn(true);
                 setUser(user);
+                setIsUrbanPlanner(user.role === 'urban_planner');
                 return user;
             }
         } catch (err) {
-            console.error("Errore nel login:", err.message);
+            console.error("Login error:", err.message);
             return null;
         }
     };
 
     const handleLogout = async () => {
         try {
-            if (loggedIn) {
-                const response = await AuthAPI.logOut();
-                if (response) {
-                    setLoggedIn(false);
-                    setUser(null);  
-                    navigate('/login');
-                }
-            } else {
+            const response = await AuthAPI.logOut();
+            if (response) {
+                setLoggedIn(false);
+                setUser(null);
+                setIsUrbanPlanner(false);
                 navigate('/login');
             }
         } catch (err) {
-            console.error("Errore nel logout:", err.message);
+            console.error("Logout error:", err.message);
         }
     };
 
@@ -76,10 +74,42 @@ function App() {
 
     return (
         <Routes>
-            <Route path="/" element={loggedIn ? <Navigate replace to='/homePage' /> : <Navigate replace to='/login' />} />
-            <Route path='/homePage' element={loggedIn ? <HomePage loggedIn={loggedIn} role={user?.role} handleLogout={handleLogout}/> : <PageNotFound/> }/>
-            <Route path='/login' element={loggedIn ? <Navigate replace to='/' /> : <Login login={handleLogin} />} />
-            {/*<Route path='/link' element={<Link />} /> {/* Add the Link component route */}
+            {/* Layout with Header */}
+            <Route
+                element={
+                    <>
+                        <Header
+                            loggedIn={loggedIn}
+                            handleLogout={handleLogout}
+                            isUrbanPlanner={isUrbanPlanner}
+                        />
+                        <Outlet />
+                    </>
+                }
+            >
+                {/* Routes */}
+                <Route path="/" element={<Navigate replace to={loggedIn ? '/map' : '/login'} />} />
+                <Route
+                    path="/homePage"
+                    element={
+                        loggedIn ? (
+                            <HomePage
+                                loggedIn={loggedIn}
+                                role={user?.role}
+                                handleLogout={handleLogout}
+                            />
+                        ) : (
+                            <PageNotFound />
+                        )
+                    }
+                />
+                <Route
+                    path="/login"
+                    element={loggedIn ? <Navigate replace to="/" /> : <Login login={handleLogin} />}
+                />
+                <Route path="/map" element={<Map role={user?.role} />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Route>
         </Routes>
     );
 }
