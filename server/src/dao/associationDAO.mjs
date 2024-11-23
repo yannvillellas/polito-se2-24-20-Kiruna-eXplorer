ffa68c4d6418d0d755b490b4defaa843e4e57e7a
Matteo D'Alicarnasso s327075 <s327075@studenti.polito.it>
2024-11-23 10:33:21 +0100

    fix sonarcloud problem

--- server/src/dao/associationDAO.mjs
+++ server/src/dao/associationDAO.mjs
@@ -84,21 +84,33 @@ export const UpdateAssociation = (association) => {
 
 /* to test */
 export const CheckAssociation = (association)=>{
-    return new Promise(async (resolve, reject) => {
-        try {
-            const typeId = await getTypeIdByType(association.type);
-            const sql = 'SELECT * FROM Association WHERE (doc1=? OR doc1=?) AND (doc2=? OR doc2=?) AND typeId=?';
-            db.all(sql, [parseInt(association.doc1, 10), parseInt(association.doc2, 10), parseInt(association.doc1, 10), parseInt(association.doc2, 10),parseInt(typeId, 10)], (err,rows) => {
-                if (err) {
-                    console.log(err)
-                    reject(err);
-                } else {
-                    console.log(rows)
-                    resolve(rows); //if rows is undefined the association desn't exist
-                }
-            });
-        } catch (err) {
-            reject(err);
-        }
+    return new Promise((resolve, reject) => {
+        const executeAsyncLogic = async () => {
+            try {
+                const typeId = await getTypeIdByType(association.type);
+                const sql = 'SELECT * FROM Association WHERE (doc1=? OR doc1=?) AND (doc2=? OR doc2=?) AND typeId=?';
+                
+                db.all(sql, [
+                    parseInt(association.doc1, 10),
+                    parseInt(association.doc2, 10),
+                    parseInt(association.doc1, 10),
+                    parseInt(association.doc2, 10),
+                    parseInt(typeId, 10)
+                ], (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                        reject(err);
+                    } else {
+                        console.log(rows);
+                        resolve(rows); // Se `rows` è undefined, l'associazione non esiste
+                    }
+                });
+            } catch (err) {
+                reject(err);
+            }
+        };
+
+        // Chiama la logica asincrona
+        executeAsyncLogic();
     });
 }
