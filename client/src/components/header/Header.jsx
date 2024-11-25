cb9c1cf5ac8646b9180e323060506a98a3fadde1
ChristianGalipo1 <s331996@studenti.polito.it>
2024-11-25 18:58:17 +0100

    fix header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,29 +1,37 @@
 import './header.css';
 import { Link, useNavigate } from 'react-router-dom';
+import logo from './logoKiruna.png';
 
 function Header(props) {
-    const navigate = useNavigate();
-    
+  const navigate = useNavigate();
+
   return (
     <div className="header-wrapper">
-      <div className='container-fluid'>
-          <nav>
-          <label htmlFor="check" className="checkbtn">
-              <i className="fa-solid fa-bars"></i>
-          </label>
-
-          <label className="logo">Kiruna-Express</label>
-
-          <ul>
-              <li><Link to="/homePage" >Map</Link></li>
-              {props.isUrbanPlanner && <li><Link to="/documentPage" >Documents</Link></li>}
-              {props.loggedIn ?   <button onClick={props.handleLogout}>Logout</button>:
-                                  <button onClick={()=>{navigate('/login')}}>Login</button>}
-          </ul>
+      <div className="container-fluid">
+        <div className="header-left">
+          <img
+            src={logo}
+            alt="Kiruna-Express Logo"
+            className="logo-img"
+            onClick={() => navigate('/homePage')}
+          />
+          <span className="separator">|</span>
+          <nav className="header-center">
+            <ul>
+              <li><Link to="/homePage">Map</Link></li>
+              {props.isUrbanPlanner && <li><Link to="/documentPage">Documents</Link></li>}
+            </ul>
           </nav>
+        </div>
+        <div className="header-right">
+          {props.loggedIn ? (
+            <button onClick={props.handleLogout} className="logout-btn">Logout</button>
+          ) : (
+            <button onClick={() => navigate('/login')} className="login-btn">Login</button>
+          )}
+        </div>
       </div>
-  </div>
-
+    </div>
   );
 }
 
