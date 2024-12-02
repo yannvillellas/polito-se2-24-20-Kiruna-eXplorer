e19f580621373225107a1feb65063ed093816bfb
Ludwig Lenzini s331395 <s331395@studenti.polito.it>
2024-12-02 09:11:58 +0100

    fix

--- client/src/api/associationAPI.js
+++ client/src/api/associationAPI.js
@@ -31,7 +31,7 @@ const createAssociation = async (association) => {
 export const getAllAssociations = async () => {
   console.log("Sono in associationAPI, ho ricevuto la richiwsra di getAllAssociations");
   try {
-    const response = await fetch(SERVER_URL, { method: 'GET', headers: { 'Cache-Control': 'no-cache' } });
+    const response = await fetch(SERVER_URL, { method: 'GET' });
     console.log("Sono in associationAPI, getAllAssociations, ho ricevuto la risposta di getAllAssociations", response);
     if (!response.ok) throw new Error('Failed to fetch associations');
     return await response.json();
@@ -48,7 +48,7 @@ export const getAllAssociations = async () => {
 const getLinkTypes = async () => {
   try {
     console.log("Sono in associationAPI, getLinkTypes, ho ricevuto la richiesta di getLinkTypes");  
-    const response = await fetch(`http://localhost:3001/api/linkTypes`, { method: 'GET', headers: { 'Cache-Control': 'no-cache' } });
+    const response = await fetch(`http://localhost:3001/api/linkTypes`, { method: 'GET'});
     console.log("Sono in associationAPI, getLinkTypes, ho ricevuto la risposta di getLinkTypes", response);
     if (!response.ok) throw new Error('Failed to fetch link types');
     return await response.json();
@@ -62,7 +62,7 @@ const getLinkTypes = async () => {
 const getAssociationsByDocId = async (docId) => {
   try {
     console.log("Sono in associationAPI ho ricevuto docId:", docId);
-    const response = await fetch(`${SERVER_URL}/${docId}`, { method: 'GET', headers: { 'Cache-Control': 'no-cache' } });
+    const response = await fetch(`${SERVER_URL}/${docId}`, { method: 'GET' });
     console.log("Sono in associationAPI, getAssociationsByDocId, ho ricevuto la risposta di getAssociationsByDocId", response);
     if (!response.ok) throw new Error('Failed to fetch associations');
     return await response.json();
