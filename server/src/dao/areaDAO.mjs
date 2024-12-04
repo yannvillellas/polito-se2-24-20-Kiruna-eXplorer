73b66784c759f21b9330883bb47d0a4b2162d553
yannvillellas <yvillellas@gmail.com>
2024-12-04 17:36:51 +0100

    fix 4 level

--- server/src/dao/areaDAO.mjs
+++ server/src/dao/areaDAO.mjs
@@ -1,101 +1,83 @@
 import { db } from "../database/db.mjs"
 import { Area, AreaAssociation } from "../models/areas.mjs"
 
-
-export const addArea = (docId, areaType, coordinates) => {
+const insertAreaAssociation = (areaId, docId) => {
     return new Promise((resolve, reject) => {
-        // Prima di inserire l'area controllo il caso in cui ci sia gia' (caso di set pre-existing area)
-        // lo vedo se c'è già areaType e coordinates
-        db.get("SELECT areaId FROM Area WHERE areaType = ? AND coordinates = ?", [areaType, coordinates], (err, row) => {
+        db.run("INSERT INTO AreaAssociation (areaId, docId) VALUES (?, ?)", [areaId, docId], function (err) {
             if (err) {
                 reject(err);
             } else {
-                if (row) {
- 
-                    // Ed aggiungi row.areaId a AreaAssociation
-                    db.run("INSERT INTO AreaAssociation (areaId, docId) VALUES (?, ?)",
-                        [
-                            row.areaId,
-                            docId
-                        ],
-                        function (err) {
-                            if (err) {
-                                reject(err);
-                            } else {
-                                resolve(row.areaId);
-                            }
-                        }
-                    );
+                resolve(areaId);
+            }
+        });
+    });
+};
 
+const insertArea = (areaType, coordinates) => {
+    return new Promise((resolve, reject) => {
+        db.run("INSERT INTO Area (areaType, coordinates) VALUES (?, ?)", [areaType, coordinates], function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(this.lastID);
+            }
+        });
+    });
+};
 
-                } else {
-                    // Altrimenti inserisco l'area
-                    db.run("INSERT INTO Area (areaType, coordinates) VALUES (?, ?)",
-                        [
-                            areaType,
-                            coordinates
-                        ],
-                        function (err) { // I need it for enable this.lastID
-                            if (err) {
-                                reject(err);
-                            } else {
-                                const areaId = this.lastID;
-                                db.run("INSERT INTO AreaAssociation (areaId, docId) VALUES (?, ?)",
-                                    [
-                                        areaId,
-                                        docId
-                                    ],
-                                    function (err) {
-                                        if (err) {
-                                            reject(err);
-                                        } else {
-                                            resolve(areaId);
-                                        }
-                                    }
-                                );
-
-                            }
-                        }
-                    );
+export const addArea = (docId, areaType, coordinates) => {
+    return new Promise((resolve, reject) => {
+        db.get("SELECT areaId FROM Area WHERE areaType = ? AND coordinates = ?", [areaType, coordinates], async (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                try {
+                    let areaId;
+                    if (row) {
+                        areaId = row.areaId;
+                    } else {
+                        areaId = await insertArea(areaType, coordinates);
+                    }
+                    await insertAreaAssociation(areaId, docId);
+                    resolve(areaId);
+                } catch (error) {
+                    reject(error);
                 }
             }
         });
+    });
+};
 
+export const listAreas = () => {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM Area", (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (rows) {
+                    const areas = rows.map(row => new Area(row.areaId, row.areaType, JSON.parse(row.coordinates)));
+                    resolve(areas);
+                } else {
+                    resolve([]);
+                }
+            }
+        });
     });
 }
 
-
-export const listAreas = () => {
-        return new Promise((resolve, reject) => {
-            db.all("SELECT * FROM Area", (err, rows) => {
-                if (err) {
-                    reject(err);
+export const listAreaAssociations = () => {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM AreaAssociation", (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (rows) {
+                    const associations = rows.map(row => new AreaAssociation(row.areaId, row.docId));
+                    resolve(associations);
                 } else {
-                    if (rows) {
-                        const areas = rows.map(row => new Area(row.areaId, row.areaType, JSON.parse(row.coordinates)));
-                        resolve(areas);
-                    } else {
-                        resolve([]);
-                    }
+                    resolve([]);
                 }
-            });
+            }
         });
-    }
-
-
-    export const listAreaAssociations = () => {
-        return new Promise((resolve, reject) => {
-            db.all("SELECT * FROM AreaAssociation", (err, rows) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    if (rows) {
-                        const associations = rows.map(row => new AreaAssociation(row.areaId, row.docId));
-                        resolve(associations);
-                    } else {
-                        resolve([]);
-                    }
-                }
-            });
-        });
-    }
+    });
+}
